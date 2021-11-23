a99e3d65f5fd00f5fdd189944a0c9b55452926c9
Adam Cseke <cseke.adam18@gmail.com>
2021-11-23 13:40:45 +0100

    Initial Commit

---
+++ SimpleNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleNews
+//
+//  Created by Adam Cseke on 2021. 11. 23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
